0d5a5a2af33bd7a5ffae2910f2188907dbda989d
Melanie Jiang <melaniejiang42@gmail.com>
2020-12-13 17:16:23 -0500

    download sheet programmatically, add support for month filtering when importing statement transactions

--- connectors/capital_one.py
+++ connectors/capital_one.py
@@ -4,45 +4,45 @@ import pandas as pd
 from .importer import Importer
 from models.models import Account, Demand, Category, Subcategory, Party, Transaction
 
-checking_file = "/Users/mejiang/Downloads/2020-09-04_transaction_download.csv"
-saving_file = "/Users/mejiang/Downloads/2020-09-04_transaction_download-1.csv"
-discover_file = "/Users/mejiang/Downloads/DFS-Search-20200904.csv"
-freedome_file = "/Users/mejiang/Downloads/Chase1205_Activity20200701_20200901_20200904.CSV"
-sapphire_file = "/Users/mejiang/Downloads/Chase3521_Activity20200101_20200904_20200904.CSV"
+checking_file = "/Users/mejiang/Downloads/2020-12-12_360Checking...0701.csv"
+saving_file = "/Users/mejiang/Downloads/2020-12-12_360MoneyMarket...9367.csv"
+discover_file = "/Users/mejiang/Downloads/DFS-Search-20201212.csv"
+freedome_file = "/Users/mejiang/Downloads/Chase1205_Activity20200101_20201212_20201212.CSV"
+sapphire_file = "/Users/mejiang/Downloads/Chase3521_Activity20200101_20201212_20201212.CSV"
 
 # Date, Amount, Source, Description
 
 class CapitalOneCheckingImporter(Importer):
     def __init__(self):
-        Importer.__init__(self, file=checking_file, skip_rows=1)
+        Importer.__init__(self, file=checking_file, skip_rows=1, date_format='%m/%d/%y')
 
     def convert_row_to_mastersheet_entry(self, row):
         return [row[1], row[2], Account.capital_one_checking.value, row[4]]
 
 class CapitalOneSavingImporter(Importer):
     def __init__(self):
-        Importer.__init__(self, file=saving_file, skip_rows=1)
+        Importer.__init__(self, file=saving_file, skip_rows=1, date_format='%m/%d/%y')
 
     def convert_row_to_mastersheet_entry(self, row):
         return [row[1], row[2], Account.capital_one_saving.value, row[4]]
 
 class DiscoverImporter(Importer):
     def __init__(self):
-        Importer.__init__(self, file=discover_file, skip_rows=13)
+        Importer.__init__(self, file=discover_file, skip_rows=1, date_format='%m/%d/%Y')
 
     def convert_row_to_mastersheet_entry(self, row):
         return [row[0], -1 * float(row[3]), Account.discover.value, row[2]]
 
 class ChaseFreedomImporter(Importer):
     def __init__(self):
-        Importer.__init__(self, file=freedome_file, skip_rows=1)
+        Importer.__init__(self, file=freedome_file, skip_rows=1, date_format='%m/%d/%Y')
 
     def convert_row_to_mastersheet_entry(self, row):
         return [row[0], row[5], Account.freedom.value, row[2]]
 
 class ChaseSapphireImporter(Importer):
     def __init__(self):
-        Importer.__init__(self, file=sapphire_file, skip_rows=1)
+        Importer.__init__(self, file=sapphire_file, skip_rows=1, date_format='%m/%d/%Y')
 
     def convert_row_to_mastersheet_entry(self, row):
         return [row[0], row[5], Account.sapphire.value, row[2]]
--- connectors/mastersheet.py
+++ connectors/mastersheet.py
@@ -1,21 +1,55 @@
 from __future__ import print_function
 import pickle
 import os.path
+import urllib
+import shutil
+import tempfile
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.readonly']
 
 # The ID and range of a sample spreadsheet.
 EXPENSE_SHEET = '1_FsoV6f-jgPHPGCxVOVRB8OGxrRkfNEIX4zfV_gq5vI'
 TOTAL_RANGE = 'Master!A:Q'
 HEADER_RANGE = 'Master!A3:Q3'
 
+def download():
+    drive = drive_conn()
+
+    SRC_MIMETYPE = 'application/vnd.google-apps.spreadsheet'
+    DST_MIMETYPE = 'text/csv'
+
+    data = drive.files().export(fileId=EXPENSE_SHEET, mimeType=DST_MIMETYPE).execute()
+    with open(os.path.join("parser", "mastersheet.csv"), 'wb') as f:
+        f.write(data)
+
+def drive_conn():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('drive', 'v3', credentials=creds)
 
 def insert(entries):
-    service = conn()
+    service = sheet_conn()
     sheet = service.spreadsheets()
     
     headers = sheet.values().get(spreadsheetId=EXPENSE_SHEET,range=HEADER_RANGE).execute().get('values', [])[0]
@@ -46,7 +80,7 @@ def insert(entries):
                                         .get('updatedCells')))
 
 def test():
-    service = conn()
+    service = sheet_conn()
 
     # Call the Sheets API
     sheet = service.spreadsheets()
@@ -68,7 +102,7 @@ def test():
             print(row)
 
 
-def conn():
+def sheet_conn():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
